0c0b0b196f6cd048e893365b96d8c8b720ffb552
sarahh52 <166995755+sarahh52@users.noreply.github.com>
2024-04-28 14:25:42 +0300

    Update пингпонг.py
    v3

--- пингпонг.py
+++ пингпонг.py
@@ -1,5 +1,4 @@
 from pygame import * 
-
 # Создаем фон игры
 back = (200,255,255)
 win_width=600
@@ -7,26 +6,10 @@ win_height=500
 window = display.set_mode((win_width,win_height))
 window.fill(back)
 
-#Создаем логические флажки и часы для фпс
-game = True 
-finish=False 
-clock = time.Clock()
-FPS =60 
-
-#Игровой цикл 
-while game: 
-    for e in event.get():
-        if e.type==QUIT:
-            game = False 
-    display.update()
-    clock.tick(FPS)
-
-
-
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.image = transform.scale(image.load(player_image), (wight, height)) 
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
@@ -41,11 +24,67 @@ class Player(GameSprite):
         keys=key.get_pressed()
         if keys[K_w] and self.rect.y>5:
             self.rect.y-=self.speed 
-         if keys[K_s] and self.rect.y<500:
+        if keys[K_s] and self.rect.y<500:
             self.rect.y+=self.speed 
     def update_r(self):
         keys=key.get_pressed()
         if keys[K_UP] and self.rect.y>5:
             self.rect.y-=self.speed 
-         if keys[K_DOWN] and self.rect.y<500:
-            self.rect.y+=self.speed
+        if keys[K_DOWN] and self.rect.y<500:
+            self.rect.y+=self.speed 
+        
+
+
+racket1 = Player('tyty.png',30,200,4,30,100)
+racket2 = Player('tyty.png',530,200,4,30,100)
+ball = GameSprite('ball.png',200,200,4,50,50)
+#Создаем логические флажки и часы для фпс
+game = True 
+finish=False 
+clock = time.Clock()
+FPS =60 
+font.init()
+font1= font.Font(None,35)
+lose1 = font1.render('PLAYER 1 LOSE', True,(180,0,0))
+lose2 = font1.render('PLAYER 2 LOSE', True,(180,0,0))
+
+speed_x=3
+speed_y=3
+
+#Игровой цикл 
+while game: 
+    for e in event.get():
+        if e.type==QUIT:
+            game = False 
+    
+
+    if not finish:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        ball.rect.x+=speed_x
+        ball.rect.y+=speed_y
+        if ball.rect.y<0 or ball.rect.y>450:
+            speed_y*=-1
+
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speed_x*=-1 
+
+        if ball.rect.x<0:
+            finish=True 
+            window.blit(lose1,(200,200))
+            
+        if ball.rect.x>600:
+            finish=True 
+            window.blit(lose2,(200,200))
+
+
+
+        racket1.reset()
+        racket2.reset()
+    ball.reset()
+
+
+    display.update()
+    clock.tick(FPS)
